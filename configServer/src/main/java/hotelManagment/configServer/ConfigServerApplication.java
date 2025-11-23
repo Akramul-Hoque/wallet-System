a5eeadda7519ca0bd8f9bd84366f355ee89c2ad8
Akramul Hoque Nahid <akramulhoque105@gmail.com>
2025-11-23 13:27:45 +0600

    walletSystem v2

--- configServer/src/main/java/hotelManagment/configServer/ConfigServerApplication.java
+++ configServer/src/main/java/hotelManagment/configServer/ConfigServerApplication.java
@@ -3,7 +3,10 @@ package hotelManagment.configServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
